26b431f606ef014a561a94dfdb2717a4db97bb73
hyonzin <hyeonjin507@gmail.com>
2017-09-15 14:17:07 +0900

    led sign with sleep

--- led_sign.py
+++ led_sign.py
@@ -1,15 +1,32 @@
 from ev3dev.ev3 import *
+import time
 
-#Leds.set(Leds.LEFT, trigger = 'timer')
-#Leds.set(Leds.LEFT, brightness_pct = 1, delay_off = 100)
 Leds.set_color(Leds.LEFT, Leds.GREEN)
 Leds.set_color(Leds.RIGHT, Leds.RED)
-Leds.set(Leds.LEFT, brightness_pct = 1, delay_off = 3000)
-Leds.set(Leds.RIGHT, brightness_pct = 1, delay_on = 3000)
 
 ts = TouchSensor()
+sec = 0
 
 while True:
     if ts.value() == 1:
         Leds.all_off()
         break
+
+    if sec < 10:
+        Leds.set(Leds.LEFT, brightness_pct = 0)
+        Leds.set_color(Leds.RIGHT, Leds.RED)
+        Leds.set(Leds.RIGHT, brightness_pct = 1)
+    elif sec < 12:
+        Leds.set_color(Leds.LEFT, Leds.YELLOW)
+        Leds.set(Leds.LEFT, brightness_pct = 1)
+        Leds.set_color(Leds.RIGHT, Leds.YELLOW)
+        Leds.set(Leds.RIGHT, brightness_pct = 1)
+    else:
+        Leds.set_color(Leds.LEFT, Leds.GREEN)
+        Leds.set(Leds.LEFT, brightness_pct = 1)
+        Leds.set(Leds.RIGHT, brightness_pct = 0)
+
+    time.sleep(1)
+    sec += 1
+    if sec >= 22:
+        sec = 0
