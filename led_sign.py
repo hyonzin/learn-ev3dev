44ba39655a4a8e7e21239704cefc88c1fa57100c
hyonzin <hyeonjin507@gmail.com>
2017-09-15 14:29:42 +0900

    set color modified

--- led_sign.py
+++ led_sign.py
@@ -11,16 +11,22 @@ while True:
 
     if sec == 0:
         Leds.set(Leds.LEFT, brightness_pct = 0)
+
         Leds.set_color(Leds.RIGHT, Leds.RED)
         Leds.set(Leds.RIGHT, brightness_pct = 1)
+
     elif sec == 10:
         Leds.set_color(Leds.LEFT, Leds.YELLOW)
         Leds.set(Leds.LEFT, brightness_pct = 1)
+
+        Leds.set(Leds.RIGHT, brightness_pct = 0)
         Leds.set_color(Leds.RIGHT, Leds.YELLOW)
         Leds.set(Leds.RIGHT, brightness_pct = 1)
     elif sec == 12:
+        Leds.set(Leds.LEFT, brightness_pct = 0)
         Leds.set_color(Leds.LEFT, Leds.GREEN)
         Leds.set(Leds.LEFT, brightness_pct = 1)
+
         Leds.set(Leds.RIGHT, brightness_pct = 0)
 
     time.sleep(1)
