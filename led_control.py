cf0402906ec1a659680cb05b30ff64f4b816e723
hyonzin <hyeonjin507@gmail.com>
2017-09-15 13:52:50 +0900

    tuple

--- led_control.py
+++ led_control.py
@@ -4,8 +4,6 @@ import ev3dev.ev3 as ev3
 ts = ev3.TouchSensor()
 
 while True:
-    if ts.value()==1:
-        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
-    else:
-        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+    ev3.Leds.set_color(ev3.Leds.LEFT,
+                       (ev3.Leds.GREEN, ev3.Leds.RED)[ts.value()])
 
