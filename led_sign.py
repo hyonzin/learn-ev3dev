3631afc360412a8621e9eef1b59b28b747cdb90c
hyonzin <hyeonjin507@gmail.com>
2017-09-15 14:19:45 +0900

    not every sec

--- led_sign.py
+++ led_sign.py
@@ -12,16 +12,16 @@ while True:
         Leds.all_off()
         break
 
-    if sec < 10:
+    if sec == 0:
         Leds.set(Leds.LEFT, brightness_pct = 0)
         Leds.set_color(Leds.RIGHT, Leds.RED)
         Leds.set(Leds.RIGHT, brightness_pct = 1)
-    elif sec < 12:
+    elif sec == 10:
         Leds.set_color(Leds.LEFT, Leds.YELLOW)
         Leds.set(Leds.LEFT, brightness_pct = 1)
         Leds.set_color(Leds.RIGHT, Leds.YELLOW)
         Leds.set(Leds.RIGHT, brightness_pct = 1)
-    else:
+    elif sec == 12:
         Leds.set_color(Leds.LEFT, Leds.GREEN)
         Leds.set(Leds.LEFT, brightness_pct = 1)
         Leds.set(Leds.RIGHT, brightness_pct = 0)
