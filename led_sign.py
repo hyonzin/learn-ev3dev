0d9cfdcfe7fd3226e13d241b2fc06670efe95dc6
hyonzin <hyeonjin507@gmail.com>
2017-09-15 14:08:01 +0900

    led sign

---
+++ led_sign.py
@@ -0,0 +1,15 @@
+from ev3dev.ev3 import *
+
+#Leds.set(Leds.LEFT, trigger = 'timer')
+#Leds.set(Leds.LEFT, brightness_pct = 1, delay_off = 100)
+Leds.set_color(Leds.LEFT, Leds.GREEN)
+Leds.set_color(Leds.RIGHT, Leds.RED)
+Leds.set(Leds.LEFT, brightness_pct = 1, delay_off = 3000)
+Leds.set(Leds.Right, brightness_pct = 1, delay_on = 3000)
+
+ts = TouchSensor()
+
+while True:
+    if ts.value() == 1:
+        Leds.all_off()
+        break
