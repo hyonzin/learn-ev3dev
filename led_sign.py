5c677f4277147ad89a6740a87bb3dba622cd9edf
hyonzin <hyeonjin507@gmail.com>
2017-09-15 14:34:29 +0900

    don't set brightness_pct

--- led_sign.py
+++ led_sign.py
@@ -10,24 +10,14 @@ while True:
         break
 
     if sec == 0:
-        Leds.set(Leds.LEFT, brightness_pct = 0)
-
         Leds.set_color(Leds.RIGHT, Leds.RED)
-        Leds.set(Leds.RIGHT, brightness_pct = 1)
 
     elif sec == 10:
         Leds.set_color(Leds.LEFT, Leds.YELLOW)
-        Leds.set(Leds.LEFT, brightness_pct = 1)
-
-        Leds.set(Leds.RIGHT, brightness_pct = 0)
         Leds.set_color(Leds.RIGHT, Leds.YELLOW)
-        Leds.set(Leds.RIGHT, brightness_pct = 1)
     elif sec == 12:
-        Leds.set(Leds.LEFT, brightness_pct = 0)
         Leds.set_color(Leds.LEFT, Leds.GREEN)
-        Leds.set(Leds.LEFT, brightness_pct = 1)
-
-        Leds.set(Leds.RIGHT, brightness_pct = 0)
+        Leds.set_color(Leds.RIGHT, Leds.YELLO, pct=0)
 
     time.sleep(1)
     sec += 1
