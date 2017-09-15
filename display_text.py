1ae36dd8f62f4f49b6d2a8ef5b7a70654df1c336
hyonzin <hyeonjin507@gmail.com>
2017-09-15 15:04:20 +0900

    display text

---
+++ display_text.py
@@ -0,0 +1,12 @@
+from ev3dev.ev3 import *
+from time import sleep
+
+lcd = Screen()
+lcd.draw.rectangle((0,0,177,40), fill='black')
+lcd.draw.text((48,13), 'Hello, world.', fill='white')
+
+my_string = 'This text is black'
+print(lcd.draw.textsize(my_string))
+lcd.draw.text((36,80), my_string)
+lcd.update()
+sleep(20)
