d33bff5db91ceb5df8700c6e0ea1c0ddca7ff6ca
hyonzin <hyeonjin507@gmail.com>
2017-09-15 16:27:46 +0900

    button process

--- button.py
+++ button.py
@@ -3,9 +3,30 @@ from time import sleep
 
 btn = Button()
 
-while True:
-    if btn.any():
-        print(btn.process())
-        exit()
+def left(state):
+    if state:
+        print('Left button pressed')
     else:
-        sleep(0.01)
+        print('Left button released')
+
+def right(state):
+    print('Right button pressed' if state else 'Right button released')
+def up(state):
+    print('Up button pressed' if state else 'Up button released')
+def down(state):
+    print('Down button pressed' if state else 'Down button released')
+def enter(state):
+    print('Enter button pressed' if state else 'Enter button released')
+def backspace(state):
+    print('Backspace button pressed' if state else 'Backspace button released')
+
+btn.on_left = left
+btn.on_right = right
+btn.on_up = up
+btn.on_down = down
+btn.on_enter = enter
+btn.on_backspace = backspace
+
+while True:
+    btn.process()
+    sleep(0.01)
