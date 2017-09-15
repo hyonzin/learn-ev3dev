36f82223f04c1dcd3d1d420eae5fb328d29fe66f
hyonzin <hyeonjin507@gmail.com>
2017-09-15 15:33:53 +0900

    bomb

--- bmp.py
+++ bmp.py
@@ -1,8 +1,24 @@
 from ev3dev.ev3 import *
 from time import sleep
 from PIL import Image
+
 lcd = Screen()
-logo = Image.open('pics/Bomb.bmp')
-lcd.image.paste(logo, (0, 0))
-lcd.update()
-sleep(5)
+bomb_image = Image.open('pics/Bomb.bmp')
+boom_image = Image.open('pics/Boom.bmp')
+
+sec = 10
+
+while True:
+    if sec >= 0:
+        lcd.draw.text((40,120), str(sec))
+        lcd.update()
+        sleep(1)
+
+        if sec > 0:
+            lcd.image.paste(bomb_image, (0, 0))
+        else:
+            lcd.image.paste(boom_image, (0, 0))
+        lcd.update()
+        sleep(1)
+
+        sec -= 1
