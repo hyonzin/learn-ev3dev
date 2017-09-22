bef970725e7820c6f8d53c4a184cfefecf13806a
hyonzin <hyeonjin507@gmail.com>
2017-09-22 16:55:23 +0900

    go slower 9

--- 0922/go_slower.py
+++ 0922/go_slower.py
@@ -16,12 +16,10 @@ multi_us_val = 3
 e = 50 # (mm)
 
 while not ts.value():
-    distance = us.value()/10
-
     cur_speed = min(MAX_SPEED,
                     start_speed,
                     max((us.value() - e) * multi_us_val, 0))
-    print(str(cur_speed) + " " + units)
+    # print(str(cur_speed) + " " + units)
 
     mL.run_forever(speed_sp = cur_speed)
     mR.run_forever(speed_sp = cur_speed)
