14e30c25132d6e3894817ac24a2fd3fa0ef143d4
hyonzin <hyeonjin507@gmail.com>
2017-09-22 14:32:55 +0900

    rectangle

--- 0922/bangbang.py
+++ 0922/bangbang.py
@@ -5,18 +5,19 @@ mL = LargeMotor('outC')
 mR = LargeMotor('outB')
 
 def go_straight():
-    mL.run_to_rel_pos(position_sp=300, speed_sp=450)
-    mR.run_to_rel_pos(position_sp=300, speed_sp=450)
+    mL.run_to_rel_pos(position_sp=1200, speed_sp=450)
+    mR.run_to_rel_pos(position_sp=1200, speed_sp=450)
 
 def turn_right():
-    mL.run_to_rel_pos(position_sp=300, speed_sp=450)
-    mR.run_to_rel_pos(position_sp=-300, speed_sp=450)
+    mL.run_to_rel_pos(position_sp=160, speed_sp=450)
+    mR.run_to_rel_pos(position_sp=-160, speed_sp=450)
 
 def wait():
     mL.wait_while('running')
     mR.wait_while('running')
 
-go_straight()
-wait()
-turn_right()
-wait()
+for i in range(0,4):
+    go_straight()
+    wait()
+    turn_right()
+    wait()
