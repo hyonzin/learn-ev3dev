98593439007914f6aa1ed757aeb1da9f7cd44637
hyonzin <hyeonjin507@gmail.com>
2017-09-22 14:09:03 +0900

    wait while

--- 0922/motor.py
+++ 0922/motor.py
@@ -3,28 +3,32 @@ from time import sleep
 
 m = LargeMotor('outB')
 
-m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action='hold')
-sleep(2)
+m.run_to_rel_pos(position_sp=60, speed_sp=100, stop_action='hold')
+sleep(1)
 
-m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action='brake')
-sleep(2)
+m.run_to_rel_pos(position_sp=60, speed_sp=100, stop_action='brake')
+sleep(1)
 
-m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action='coast')
-sleep(2)
+m.run_to_rel_pos(position_sp=60, speed_sp=100, stop_action='coast')
+sleep(1)
 
 
-m.run_timed(time_sp=3000, speed_sp=-750)
+m.run_timed(time_sp=1000, speed_sp=-750)
 print("set speed (speed_sp) = " + str(m.speed_sp))
 print("actual speed = " + str(m.speed))
 sleep(1)
 print("actual speed = " + str(m.speed))
-sleep(4)
+sleep(1)
 
 
-m.run_timed(time_sp=3000, speed_sp=750)
+m.run_timed(time_sp=1000, speed_sp=750)
 print("set speed (speed_sp) = " + str(m.speed_sp))
 print("actual speed = " + str(m.speed))
 sleep(1)
 print("actual speed = " + str(m.speed))
-sleep(4)
+sleep(1)
 
+m.run_timed(time_sp=3000, speed_sp=-750, stop_action='brake')
+m.wait_while('running')
+
+Sound.beep()
