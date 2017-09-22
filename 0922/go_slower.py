55c71daa17a30fb53d39cf9fdad49c755f1cd5c7
hyonzin <hyeonjin507@gmail.com>
2017-09-22 16:49:17 +0900

    go slower 7

--- 0922/go_slower.py
+++ 0922/go_slower.py
@@ -10,13 +10,15 @@ ts = TouchSensor(); assert ts.connected, "Connect a touch sensor to any port"
 us.mode='US-DIST-CM'
 units = us.units
 
-m_speed = 1500
+MAX_SPEED = 1000
+m_speed = 1000
+multi_us_val = 2
 e = 50 # (mm)
 
 while not ts.value():
     distance = us.value()/10
 
-    cur_speed = min(m_speed, max(us.value() - e, 0))
+    cur_speed = min(MAX_SPEED, m_speed, max(us.value()*multi_us_val - e, 0))
     print(str(cur_speed) + " " + units)
 
     mL.run_forever(speed_sp = cur_speed)
