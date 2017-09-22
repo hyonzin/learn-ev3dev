1908e2b2efa63e2de075a740498573e79c2b6d42
hyonzin <hyeonjin507@gmail.com>
2017-09-22 16:12:57 +0900

    stop

--- 0922/ultra_sonic.py
+++ 0922/ultra_sonic.py
@@ -9,7 +9,7 @@ ts = TouchSensor(); assert ts.connected, "Connect a touch sensor to any port"
 
 us.mode='US-DIST-CM'
 units = us.units
-e = 0.1
+e = 1
 
 while not ts.value():
     distance = us.value()/10
@@ -30,3 +30,5 @@ while not ts.value():
 
 Sound.beep()
 Leds.set_color(Leds.LEFT, Leds.GREEN)
+mL.stop(stop_action="hold")
+mR.stop(stop_action="hold")
