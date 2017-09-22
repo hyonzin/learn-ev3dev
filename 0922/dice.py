ad1d4ce7469d2b2a3b48687260db5d621722e02e
hyonzin <hyeonjin507@gmail.com>
2017-09-22 13:23:17 +0900

    170922 dice

--- 0922/dice.py
+++ 0922/dice.py
@@ -1,6 +1,17 @@
+from time import sleep
+import random
+import ev3dev.fonts as fonts
 import ev3dev.ev3 as ev3
+from ev3dev.ev3 import *
+
 ts = ev3.TouchSensor()
+
+lcd = Screen()
 
 while True:
     if (ts.is_pressed):
-        print("hi")
+        lcd_str = str(random.randint(1, 6))
+        lcd.clear()
+        lcd.draw.text((60,60), lcd_str, font = fonts.load('luBS18'))
+        lcd.update()
+        sleep(1)
