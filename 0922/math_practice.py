f2d2958e9f2b760372059b76a8b8e35f1667f003
hyonzin <hyeonjin507@gmail.com>
2017-09-22 13:17:14 +0900

    math practice

---
+++ 0922/math_practice.py
@@ -0,0 +1,11 @@
+import math
+import random
+
+print(abs(-34.26))
+print(round(-34.26))
+print(round(-34.26, 1))
+
+print (math.cos(math.radians(45)))
+
+print(random.randint(-100, 100))
+print(random.random())
