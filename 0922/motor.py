2ca6196641edd3fbc3a5f4feb8e7bb65dc14089e
hyonzin <hyeonjin507@gmail.com>
2017-09-22 14:10:16 +0900

    slow

--- 0922/motor.py
+++ 0922/motor.py
@@ -13,7 +13,7 @@ m.run_to_rel_pos(position_sp=60, speed_sp=100, stop_action='coast')
 sleep(1)
 
 
-m.run_timed(time_sp=1000, speed_sp=-750)
+m.run_timed(time_sp=1000, speed_sp=-100)
 print("set speed (speed_sp) = " + str(m.speed_sp))
 print("actual speed = " + str(m.speed))
 sleep(1)
@@ -21,14 +21,14 @@ print("actual speed = " + str(m.speed))
 sleep(1)
 
 
-m.run_timed(time_sp=1000, speed_sp=750)
+m.run_timed(time_sp=1000, speed_sp=100)
 print("set speed (speed_sp) = " + str(m.speed_sp))
 print("actual speed = " + str(m.speed))
 sleep(1)
 print("actual speed = " + str(m.speed))
 sleep(1)
 
-m.run_timed(time_sp=3000, speed_sp=-750, stop_action='brake')
+m.run_timed(time_sp=3000, speed_sp=250, stop_action='brake')
 m.wait_while('running')
 
 Sound.beep()
