b08b0e586b6a68292e7ae64f961299e25c05b4a1
hyonzin <hyeonjin507@gmail.com>
2017-09-29 13:47:38 +0900

    show gyro rate with my class

--- 0929/03_gyro_rate.py
+++ 0929/03_gyro_rate.py
@@ -4,5 +4,7 @@ from hyonzin.ev3 import Ev3;
 
 robot = Ev3()
 
+initial_angle = robot.gy.value()
+
 while not robot.ts.is_pressed:
-    robot.show_text(robot.gy.value())
+    robot.show_text(robot.gy.value() - initial_angle)
