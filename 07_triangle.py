61af38b860d82ac718c86be8b2603cb965f48a2d
hyonzin <hyeonjin507@gmail.com>
2017-09-29 15:01:53 +0900

    rotation not using my class

--- 07_triangle.py
+++ 07_triangle.py
@@ -1,12 +1,41 @@
-from hyonzin.ev3 import *
+from ev3dev.ev3 import *
 
-robot = Ev3()
+mL = LargeMotor('outC')
+mR = LargeMotor('outB')
+ts = TouchSensor()
+gy = GyroSensor()
+gy.mode = 'GYRO-ANG'
 
-initial_angle = robot.gy.value()
+initial_angle = gy.value()
 constant = 10
 max_speed = 999
 min_speed= -999
+degree = 120
+go_position = 1000
+go_speed = 500
+rotate_speed = 100
+turn_count = 1
 
-while not robot.ts.is_pressed:
-    robot.go(position_sp=500)
-    robot.rotate(degree=120)
+start_angle = current_angle = gy.value()
+
+while not ts.is_pressed:
+    # Go straight
+    mL.run_to_rel_pos(position_sp=go_position, speed_sp=go_speed, stop_action='hold')
+    mR.run_to_rel_pos(position_sp=go_position, speed_sp=go_speed, stop_action='hold')
+    mL.wait_while('running')
+    mR.wait_while('running')
+
+    mL.run_forever(speed_sp= degree)
+    mR.run_forever(speed_sp=-degree)
+
+    # Turn
+    finish_angle = start_angle + degree*turn_count;
+
+    while (degree > 0 and current_angle < finish_angle) \
+            or (degree < 0 and current_angle > finish_angle):
+        current_angle = gy.value()
+        #print(str(current_angle) + " " + gy.units)
+
+    mL.stop(stop_action="hold")
+    mR.stop(stop_action="hold")
+    turn_count+=1
