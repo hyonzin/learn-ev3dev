d2c861a1477ad2a08cb35fa74be6a7090c09d84c
hyonzin <hyeonjin507@gmail.com>
2017-09-29 17:28:27 +0900

    avoid 11 wip

--- 09_avoid.py
+++ 09_avoid.py
@@ -9,11 +9,11 @@ gy.mode = 'GYRO-ANG'
 us = UltrasonicSensor()
 us.mode='US-DIST-CM'
 
-go_speed = 100
+go_speed = 500
 rotate_speed = 50
 
 initial_angle = gy.value()
-short_distance = 500
+short_distance = 200
 
 def limit_speed(speed):
     max_speed = 999
@@ -30,31 +30,33 @@ def turn_to(angle):
         mL.run_forever(speed_sp=-rotate_speed)
         mR.run_forever(speed_sp= rotate_speed)
 
-    while (angle > start_angle and angle > current_angle) \
-            or (angle < start_angle and angle < current_angle):
+    while not ts.is_pressed \
+            and ((angle > start_angle and angle > current_angle) \
+            or   (angle < start_angle and angle < current_angle)):
         current_angle = gy.value()
 
-def go(distance = short_distance):
+def go(depth, distance=short_distance):
     moved_distance=0
 
-    while moved_distance < distance:
+    while not ts.is_pressed and moved_distance < distance:
         mL.run_forever(speed_sp=limit_speed(go_speed))
         mR.run_forever(speed_sp=limit_speed(go_speed))
 
         current_angle = gy.value()
-        if us.value() < 200:
+        if us.value() < 400:
             turn_to(current_angle+90)
-            go()
+
+            go(depth=depth+1)
             turn_to(current_angle)
-            go()
+            go(depth=depth+1)
             turn_to(current_angle-90)
-            go()
+            go(depth=depth+1)
             turn_to(current_angle)
 
         moved_distance += 1
 
 while not ts.is_pressed:
-    go(1)
+    go(depth=0, distance=1)
 
 mL.stop(stop_action="hold")
 mR.stop(stop_action="hold")
