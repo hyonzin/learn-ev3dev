32ab278ee6bc963ff013360bade69c93bf2e188f
hyonzin <hyeonjin507@gmail.com>
2017-09-29 14:38:36 +0900

    rotate rectangle

---
+++ 06_rectangle.py
@@ -0,0 +1,13 @@
+from hyonzin.ev3 import *
+
+robot = Ev3()
+
+initial_angle = robot.gy.value()
+constant = 10
+max_speed = 999
+min_speed= -999
+
+while not robot.ts.is_pressed:
+    robot.go(position_sp=500)
+    robot.rotate(degree=90)
+
