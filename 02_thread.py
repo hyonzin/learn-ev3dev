d503337f62125321289cb67b4660e77e1a9adc11
hyonzin <hyeonjin507@gmail.com>
2017-10-20 13:15:17 +0900

    multi thread

---
+++ 02_thread.py
@@ -0,0 +1,22 @@
+from ev3dev.ev3 import *
+from time import sleep
+from threading import Thread
+ts = TouchSensor()
+
+assert ts.connected, "connect touch sensor"
+
+def playtone():
+    while True:
+        Sound.tone(1000, 200).wait()
+        sleep(0.5)
+
+t = Thread(target=playtone)
+t.start()
+
+for i in range(5):
+    while ts.value() == 0:
+        sleep(0.01)
+    while ts.value() == 1:
+        sleep(0.01)
+
+Sound.beep()
